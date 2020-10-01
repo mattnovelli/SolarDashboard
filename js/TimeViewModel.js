3dc600f2b71ee697e4365f080fcc7659bf013eb2
Ryan Blackman <rblackman24@gmail.com>
2020-10-01 09:44:50 -0500

    update screen size, urls and general design

--- js/TimeViewModel.js
+++ js/TimeViewModel.js
@@ -1,30 +1,33 @@
+const timeUpdateIntervalSeconds = 10;
+const timeSyncIntervalMinutes = 10;
 /* indent: 2 */
-var TimeViewModel = (function () {
-  'use strict';
-  return function Constructor() {
-    var self = this;
-    self.time = ko.observable(moment());
-    self.formattedTime = ko.computed(function () { return self.time().format("h:mm:ss a"); });
+const TimeViewModel = (function () {
+	'use strict';
+	return function Constructor() {
+		var self = this;
+		self.time = ko.observable(moment());
+		self.formattedTime = ko.computed(function () { return self.time().format("h:mm"); });
+		self.ampm = ko.computed(function () { return self.time().format("a"); })
 
-    self.fetch = function () {
-      $.ajax({
-        url: 'https://mtdweb.cumtd.com/api/time',
-        dataType: 'JSON',
-        contentType: 'text/plain',
-        xhrFields: {
-          withCredentials: false
-        }
-      }).then(function (data) {
-        self.time(moment(data));
-      });
-    };
+		self.fetch = function () {
+			$.ajax({
+				url: 'https://mtdweb.mtd.org/api/time',
+				dataType: 'JSON',
+				contentType: 'text/plain',
+				xhrFields: {
+					withCredentials: false
+				}
+			}).then(function (data) {
+				self.time(moment(data));
+			});
+		};
 
-    window.setInterval(function () {
-      self.time(self.time().add(1, 'seconds'));
-    }, 1000);
+		window.setInterval(function () {
+			self.time(self.time().add(timeUpdateIntervalSeconds, 'seconds'));
+		}, timeUpdateIntervalSeconds * 1000);
 
-    window.setInterval(self.fetch, 600000); // 10 minutes
-    self.fetch();
+		window.setInterval(self.fetch, timeSyncIntervalMinutes * 60 * 1000);
+		self.fetch();
 
-  };
+	};
 }());
--- js/WeatherViewModel.js
+++ js/WeatherViewModel.js
@@ -1,36 +1,38 @@
 /* indent: 2 */
 var WeatherViewModel = (function () {
-  'use strict';
-  return function Constructor() {
+	'use strict';
+	return function Constructor() {
 
-    var self = this;
+		var self = this;
 
-    self.temp = ko.observable('N/A');
-    self.forcast = ko.observable('N/A');
-    self.unavailable = ko.computed(function () { return self.temp() === 'N/A' || self.forcast() === 'N/A'; });
+		self.temp = ko.observable('N/A');
+		self.forecast = ko.observable('N/A');
+		self.unavailable = ko.computed(function () { return self.temp() === 'N/A' || self.forecast() === 'N/A'; });
 
-    self.fetch = function () {
-      $.ajax({
-        url: 'https://mtdweb.cumtd.com/api/weather/current',
-        dataType: 'JSON',
-        contentType: 'text/plain',
-        xhrFields: {
-          withCredentials: false
-        }
-      }).then(function (data) {
-        self.temp(data.Temperature);
-        self.forcast(data.Conditions);
-        if (self.unavailable()) {
-          window.setTimeout(self.fetch, 15000);
-        }
-      }, function () {
-        self.temp('N/A');
-        self.forcast = 'N/A';
-      });
-    };
+		self.fetch = function () {
+			$.ajax({
+				url: 'https://mtdweb.mtd.org/api/weather/current',
+				dataType: 'JSON',
+				contentType: 'text/plain',
+				xhrFields: {
+					withCredentials: false
+				}
+			}).then(function (data) {
+				console.log('weather', data);
+				self.temp(data.Temperature);
+				self.forecast(data.Conditions.toLowerCase());
+				if (self.unavailable()) {
+					window.setTimeout(self.fetch, 15000);
+				}
+			}, function (error) {
+				console.warn('weather error', error);
+				self.temp('N/A');
+				self.forecast = 'N/A';
+			});
+		};
 
-    window.setInterval(self.fetch, 1200000); // 20 minutes
-    self.fetch();
+		window.setInterval(self.fetch, 1200000); // 20 minutes
+		self.fetch();
 
-  };
-} ());
+	};
+}());
